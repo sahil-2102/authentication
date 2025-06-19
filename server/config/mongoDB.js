de8af1402973becd85ce7685729d0024eca92bb9
Sahil <sk4386840@gmail.com>
2025-06-19 14:15:21 +0530

    minor changes

--- server/config/mongoDB.js
+++ server/config/mongoDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
     } catch (error) {
         console.log("Error while connectig to the database ", error.message);
     }
