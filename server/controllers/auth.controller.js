1ddcb2f8264c2391e2e62d882e90766029a8a664
Sahil <sk4386840@gmail.com>
2025-06-19 15:53:07 +0530

    create logout function

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -47,3 +47,16 @@ export const login = async (req, res) => {
         res.json({success: false, message: error.message});
     }
 }
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+        })
+        return res.json({success: true, message: "Logged out!"});
+    } catch (error) {
+        return res.json({success: false, message: error.message});
+    }
+}
