4e83c3c5ef0716f93feefe2b20b8a410fab9677f
Sahil <sk4386840@gmail.com>
2025-06-19 15:42:14 +0530

    create user logout function and add a few changes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -20,7 +20,29 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7d in milliseconds
         })
-
+        return res.json({success: true});
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
+}
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password) return res.json({success: false, message: "Both fields are required!"});
+    try {
+        const user  = await userModel.findOne({email});
+        if(!user) return res.json({success: false, message: "Invalid credentials!"});
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.json({success: false, message: "Invalid credentials!"});
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+            expiresIn: '7d'
+        });
+        res.cookie('token',token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+        return res.json({success: true});
     } catch (error) {
         res.json({success: false, message: error.message});
     }
