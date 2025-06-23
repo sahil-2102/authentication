53a8c21bc3b907a5b75317cf3e1789076f286c47
Sahil <sk4386840@gmail.com>
2025-06-23 13:23:42 +0530

    navigate logic in navbar button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import { useNavigate } from 'react-router-dom'
 import {assets} from '../assets/assets.js'
 const Navbar = () => {
+    const navigate = useNavigate();
   return (
     <div className='w-full flex justify-between p-4 sm:p-6 sm:px-24 items-center absolute top-0'>
         <img src={assets.logo} alt="Logo" className='w-28 sm:w-32' />
-        <button className='border border-gray-500 px-6 py-2 text-gray-800 rounded-full flex justify-center items-center gap-2 hover:bg-gray-100 transition-all'>Login <img src={assets.arrow_icon} alt="login button" /></button>
+        <button onClick={() => navigate('/login')} className='border border-gray-500 px-6 py-2 text-gray-800 rounded-full flex justify-center items-center gap-2 hover:bg-gray-100 transition-all'>Login <img src={assets.arrow_icon} alt="login button" /></button>
     </div>
   )
 }
