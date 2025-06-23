1a6434184bcee2861f2f15468693c98e33a5dd64
Sahil <sk4386840@gmail.com>
2025-06-23 16:17:23 +0530

    navbar logic complete

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,6 +17,20 @@ const Navbar = () => {
             toast.error(error.message);
         }
     }
+    const sendVerificationOtp = async () => {
+        try {
+            axios.defaults.withCredentials = true;
+            const {data} = await axios.post(backendUrl + '/api/auth/send-verify-otp');
+            if(data.success){
+                navigate('/email-verify');
+                test.success(data.message);
+            }else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
   return (
     <div className='w-full flex justify-between p-4 sm:p-6 sm:px-24 items-center absolute top-0'>
         <img src={assets.logo} alt="Logo" className='w-28 sm:w-32' />
@@ -24,7 +38,7 @@ const Navbar = () => {
         <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>{userData.name[0].toUpperCase()}
         <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
             <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
-                {!userData.isAccountVerified && <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify Email</li>}
+                {!userData.isAccountVerified && <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify Email</li>}
                 <li
                     onClick={logout}
                 className='py-1 px-2 pr-10 hover:bg-gray-200 cursor-pointer'>Logout</li>
