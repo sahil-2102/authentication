46cd0c7ca6ce6d5670b51bfc771baf3e24acdf35
Sahil <sk4386840@gmail.com>
2025-06-23 15:01:38 +0530

    login and register logic complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,12 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import ResetPassword from "./pages/ResetPassword"
 import { Routes, Route } from "react-router-dom"
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 const App = () => {
   return (
     <div className="">
+      <ToastContainer/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element ={<Login/>} />
